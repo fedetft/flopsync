976e4ffdb54f97ad0ad6a1a2f0145bdc3eb528be
Terraneo Federico <fede.tft@hotmail.it>
2013-09-18 20:15:33 +0200

    Removed old Flopsync controllers, added preinitialization

--- flopsync2.cpp
+++ flopsync2.cpp
@@ -265,9 +265,21 @@ OptimizedRampFlopsync::OptimizedRampFlopsync() { reset(); }
 
 pair<int,int> OptimizedRampFlopsync::computeCorrection(int e)
 {
-    //u(k)=2u(k-1)-u(k-2)+2.75e(k)-2.984375e(k-1)+e(k-2) with values kept multiplied by 64
-    //int u=2*uo-uoo+176*e-191*eo+64*eoo;
+    switch(init)
+    {
+        case 0:
+            init=1;
+            eo=e;
+            uo=2*64*e;
+            uoo=64*e;
+            return make_pair(2*e,w); //One step of a deadbeat controller
+        case 1:
+            init=2;
+            eo=0;
+            uo/=2;
+    }
     
+    //alpha=3/8
     //u(k)=2u(k-1)-u(k-2)+2.25e(k)-2.859375e(k-1)+e(k-2) with values kept multiplied by 64
     int u=2*uo-uoo+144*e-183*eo+64*eoo;
     uoo=uo;
@@ -277,7 +289,6 @@ pair<int,int> OptimizedRampFlopsync::computeCorrection(int e)
 
     int sign=u>=0 ? 1 : -1;
     int uquant=(u+32*sign)/64;
-    
     
     //Scale numbers if VHT is enabled to prevent overflows
     int wMax=w/scaleFactor;
@@ -307,6 +318,12 @@ pair<int,int> OptimizedRampFlopsync::computeCorrection(int e)
 
 pair<int,int> OptimizedRampFlopsync::lostPacket()
 {
+    if(init==1)
+    {
+        init=2;
+        eo=0;
+        uo/=2;
+    }
     //Double receiver window on packet loss, still clamped to max value
     dw=min<int>(2*dw,w/scaleFactor);
     return make_pair(getClockCorrection(),scaleFactor*dw);
@@ -316,6 +333,7 @@ void OptimizedRampFlopsync::reset()
 {
     uo=uoo=eo=eoo=sum=squareSum=count=0;
     dw=w/scaleFactor;
+    init=0;
 }
 
 int OptimizedRampFlopsync::getClockCorrection() const
@@ -324,128 +342,6 @@ int OptimizedRampFlopsync::getClockCorrection() const
     //do is to reuse the past correction value
     int sign=uo>=0 ? 1 : -1;
     return (uo+32*sign)/64;
-}
-
-//
-// class OptimizedFlopsync
-//
-
-OptimizedFlopsync::OptimizedFlopsync() { reset(); }
-
-pair<int,int> OptimizedFlopsync::computeCorrection(int e)
-{
-    //u(k)=u(k-1)+1.46875*e(k)-e(k-1) with values kept multiplied by 32
-    int u=uo+47*e-32*eo;
-
-    //The controller output needs to be quantized, but instead of simply
-    //doing u/4 which rounds towards the lowest number use a slightly more
-    //advanced algorithm to round towards the closest one, as when the error
-    //is close to +/-1 timer tick this makes a significant difference.
-    int sign=u>=0 ? 1 : -1;
-    int uquant=(u+16*sign)/32;
-    
-    //Adaptive state quantization, while the output always needs to be
-    //quantized, the state is only quantized if the error is zero
-    uo= e==0 ? 32*uquant : u;
-    eo=e;
-    
-    //Scale numbers if VHT is enabled to prevent overflows
-    int wMax=w/scaleFactor;
-    int wMin=minw/scaleFactor;
-    e/=scaleFactor;
-    
-    //Update receiver window size
-    sum+=e*fp;
-    squareSum+=e*e*fp;
-    if(++count>=numSamples)
-    {
-        //Variance computed as E[X^2]-E[X]^2
-        int average=sum/numSamples;
-        int var=squareSum/numSamples-average*average/fp;
-        //Using the Babylonian method to approximate square root
-        int stddev=var/7;
-        for(int j=0;j<3;j++) if(stddev>0) stddev=(stddev+var*fp/stddev)/2;
-        //Set the window size to three sigma
-        int winSize=stddev*3/fp;
-        //Clamp between min and max window
-        dw=max<int>(min<int>(winSize,wMax),wMin);
-        sum=squareSum=count=0;
-    }
-
-    return make_pair(uquant,scaleFactor*dw);
-}
-
-pair<int,int> OptimizedFlopsync::lostPacket()
-{
-    //Double receiver window on packet loss, still clamped to max value
-    dw=min<int>(2*dw,w/scaleFactor);
-    return make_pair(getClockCorrection(),scaleFactor*dw);
-}
-
-void OptimizedFlopsync::reset()
-{
-    uo=eo=sum=squareSum=count=0;
-    dw=w/scaleFactor;
-}
-
-int OptimizedFlopsync::getClockCorrection() const
-{
-    //Error measure is unavailable if the packet is lost, the best we can
-    //do is to reuse the past correction value
-    int sign=uo>=0 ? 1 : -1;
-    return (uo+16*sign)/32;
-}
-
-//
-// class OptimizedDeadbeatFlopsync
-//
-
-OptimizedDeadbeatFlopsync::OptimizedDeadbeatFlopsync() { reset(); }
-
-pair<int,int> OptimizedDeadbeatFlopsync::computeCorrection(int e)
-{
-    //u(k)=u(k-1)+2*e(k)-e(k-1)
-    int u=uo+2*e-eo;
-    uo=u;
-    eo=e;
-    
-    //Scale numbers if VHT is enabled to prevent overflows
-    int wMax=w/scaleFactor;
-    int wMin=minw/scaleFactor;
-    e/=scaleFactor;
-    
-    //Update receiver window size
-    sum+=e*fp;
-    squareSum+=e*e*fp;
-    if(++count>=numSamples)
-    {
-        //Variance computed as E[X^2]-E[X]^2
-        int average=sum/numSamples;
-        int var=squareSum/numSamples-average*average/fp;
-        //Using the Babylonian method to approximate square root
-        int stddev=var/7;
-        for(int j=0;j<3;j++) if(stddev>0) stddev=(stddev+var*fp/stddev)/2;
-        //Set the window size to three sigma
-        int winSize=stddev*3/fp;
-        //Clamp between min and max window
-        dw=max<int>(min<int>(winSize,wMax),wMin);
-        sum=squareSum=count=0;
-    }
-
-    return make_pair(u,scaleFactor*dw);
-}
-
-pair<int,int> OptimizedDeadbeatFlopsync::lostPacket()
-{
-    //Double receiver window on packet loss, still clamped to max value
-    dw=min<int>(2*dw,w/scaleFactor);
-    return make_pair(getClockCorrection(),scaleFactor*dw);
-}
-
-void OptimizedDeadbeatFlopsync::reset()
-{
-    uo=eo=sum=squareSum=count=0;
-    dw=w/scaleFactor;
 }
 
 //
