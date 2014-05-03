9cf9602709bebe4609eab1790e8ba3878b43455f
Lugi Rinaldi <luigi.rnl@gmail.com>
2014-05-03 17:21:55 +0200

    Fixed deploy script.

--- deploy/mkpackage.pl
+++ deploy/mkpackage.pl
@@ -19,11 +19,16 @@ my @configkeys=('experiment_name','experiment_time','sync_period',
 				'relative_clock','interactive_rootnode','event_timestamping',
 				'vht','sense_temperature','send_timestamps','glossy','multi_hop',
 				'sync_by_wire','comb',
-				'node0_file','node0_hop','node1_file','node1_hop',
-				'node2_file','node2_hop','node3_file','node3_hop',
-				'node4_file','node4_hop','node5_file','node5_hop',
-				'node6_file','node6_hop','node7_file','node7_hop',
-				'node8_file','node8_hop','node9_file','node9_hop');
+				'node0_enable','node0_controller','node0_file','node0_hop',
+				'node1_enable','node1_controller','node1_file','node1_hop',
+				'node2_enable','node2_controller','node2_file','node2_hop',
+				'node3_enable','node3_controller','node3_file','node3_hop',
+				'node4_enable','node4_controller','node4_file','node4_hop',
+				'node5_enable','node5_controller','node5_file','node5_hop',
+				'node6_enable','node6_controller','node6_file','node6_hop',
+				'node7_enable','node7_controller','node7_file','node7_hop',
+				'node8_enable','node8_controller','node8_file','node8_hop',
+				'node9_enable','node9_controller','node9_file','node9_hop');
 @configkeys=sort(@configkeys);
 my @sortedconfig=sort(keys(%config));
 #  If arrays not equal, some coniguration parameters are missing
@@ -38,7 +43,7 @@ mkdir $config{'experiment_name'} or die 'experiment_name: directory exists';
 # secondhop=wheter or not the program needs #define SECOND_HOP
 sub build
 {
-	my ($srcfile, $binfile, $elffile, $hop)=@_;
+	my ($srcfile, $binfile, $elffile, $hop, $controller)=@_;
 
 	# Step 1: create a Makefile with 'SRC := $srcfile \'
 	# Note that all the lines but 'SRC :=' are not modified, so there
@@ -109,6 +114,11 @@ sub build
 			{
 			    print $outfile "$1'0'$2\n";
 			}
+		} elsif(/^(const unsigned char controller=).*(;)/) {
+			if (!($controller<0 || $controller>3))
+			{
+			    print $outfile "$1$controller$2\n";
+			}
 		} elsif(/^(const unsigned long long nominalPeriod=static_cast<unsigned long long>\().*(\*hz\+0.5f\);)/) {
 			print $outfile "$1$config{sync_period}$2\n";
 		} else { print $outfile $_; }
@@ -137,16 +147,16 @@ sub build
 
 # Build files for all three nodes
 `make clean 1>/dev/null 2>/dev/null`; # Make sure the build directory is clean
-build($config{'node0_file'},'node0.bin','node0.elf',$config{'node0_hop'});
-build($config{'node1_file'},'node1.bin','node1.elf',$config{'node1_hop'});
-#build($config{'node2_file'},'node2.bin','node2.elf',$config{'node2_hop'});
-#build($config{'node3_file'},'node3.bin','node3.elf',$config{'node3_hop'});
-#build($config{'node4_file'},'node4.bin','node4.elf',$config{'node4_hop'});
-#build($config{'node5_file'},'node5.bin','node5.elf',$config{'node5_hop'});
-#build($config{'node6_file'},'node6.bin','node6.elf',$config{'node6_hop'});
-#build($config{'node7_file'},'node7.bin','node7.elf',$config{'node7_hop'});
-#build($config{'node8_file'},'node8.bin','node8.elf',$config{'node8_hop'});
-#build($config{'node9_file'},'node9.bin','node9.elf',$config{'node9_hop'});
+if($config{'node0_enable'}) {build($config{'node0_file'},'node0.bin','node0.elf',$config{'node0_hop'},$config{'node0_controller'});}
+if($config{'node1_enable'}) {build($config{'node1_file'},'node1.bin','node1.elf',$config{'node1_hop'},$config{'node1_controller'});}
+if($config{'node2_enable'}) {build($config{'node2_file'},'node2.bin','node2.elf',$config{'node2_hop'},$config{'node2_controller'});}
+if($config{'node3_enable'}) {build($config{'node3_file'},'node3.bin','node3.elf',$config{'node3_hop'},$config{'node3_controller'});}
+if($config{'node4_enable'}) {build($config{'node4_file'},'node4.bin','node4.elf',$config{'node4_hop'},$config{'node4_controller'});}
+if($config{'node5_enable'}) {build($config{'node5_file'},'node5.bin','node5.elf',$config{'node5_hop'},$config{'node5_controller'});}
+if($config{'node6_enable'}) {build($config{'node6_file'},'node6.bin','node6.elf',$config{'node6_hop'},$config{'node6_controller'});}
+if($config{'node7_enable'}) {build($config{'node7_file'},'node7.bin','node1.elf',$config{'node7_hop'},$config{'node7_controller'});}
+if($config{'node8_enable'}) {build($config{'node8_file'},'node8.bin','node8.elf',$config{'node8_hop'},$config{'node8_controller'});}
+if($config{'node9_enable'}) {build($config{'node9_file'},'node9.bin','node9.elf',$config{'node9_hop'},$config{'node9_controller'});}
 
 # Fixup runexp.sh to set the experimen duration
 open(my $infile, '<', 'deploy/runexp.sh');
